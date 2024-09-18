868c7d115ad37be3850e41e8201516f717a96da1
Alfonso Rivera <jhon_leo_41@hotmail.com>
2024-09-18 22:10:04 +0200

    install packages

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Button from './components/ui/button.jsx';
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Button /> 
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/ui/button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const button = () => {
+  return (
+    <div>
+      Hello word
+    </div>
+  )
+}
+
+export default button
